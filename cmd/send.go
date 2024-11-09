fb890328274ccfa3eca144d058a8e1dc2f913769
Panayotis Vryonis <58812+vrypan@users.noreply.github.com>
2024-11-10 00:59:26 +0200

    Cleaned up comments

--- cmd/send.go
+++ cmd/send.go
@@ -18,14 +18,4 @@ var sendCmd = &cobra.Command{
 
 func init() {
 	rootCmd.AddCommand(sendCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// sendCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// sendCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
--- cmd/utils.go
+++ cmd/utils.go
@@ -22,22 +22,6 @@ func ParseFcURI(uri string) (*fctools.User, []string) {
 		log.Fatal("Path should start with @")
 	}
 	user := fctools.NewUser().FromFname(nil, parts[0][1:])
-
-	/*
-		var fid uint64
-		var err error
-
-		if parts[0] == "@" {
-			return 0, nil
-		}
-		if fid, err = strconv.ParseUint(parts[0][1:], 10, 64); err != nil {
-			fid, err = fctools.GetFidByFname(parts[0][1:])
-			if err != nil {
-				log.Fatalf("Error looking up %v [%v]", parts[0], err)
-			}
-		}
-		return fid, parts[1:]
-	*/
 	return user, parts[1:]
 }
 
